a8248c6c0142a41351dcec58a9a114a7f177766b
Amir <khanfaizan1532000@gmail.com>
2020-06-02 22:55:57 +0530

    Create forecast.js

---
+++ scripts/forecast.js
@@ -0,0 +1,18 @@
+const key='AZK2eovPDQa0dSTPugbCbh4W6faWy4yj';
+const getWeather = async(id)=>{
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query =`${id}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+}
+
+
+const getCity = async (city) => {
+   const base='http://dataservice.accuweather.com/locations/v1/cities/search';
+   const query=`?apikey=${key}&q=${city}`;
+   const response = await fetch(base + query);
+   const data = await response.json();
+    return data[0];
+};
+
